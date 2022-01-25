c97780c159b0957db66daf7eb42fe61962720c55
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-01-25 04:42:48 -0300

    Configured fake API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'desposit',
+          category: 'food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 function App() {
   return (
