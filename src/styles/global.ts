d2aa8da040f7dca522acbfe07ecbf7fe98672f13
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-02-07 23:02:28 -0300

    Updated text-title color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
     --blue-ligth: #6933FF;
 
     --text-body: #a8a8b3;
-    --text-title: #969CB3;
+    --text-title: #363F5F;
 
     --shape: #ffffff;
     --background: #f0f2f5;
@@ -53,5 +53,36 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
   }
 `
