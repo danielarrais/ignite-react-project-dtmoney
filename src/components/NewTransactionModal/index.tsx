73b2ca4d6f568b93a026f3207924d250f2b04df4
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-02-07 23:02:13 -0300

    Created new transaction form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import { Container, TransactionTypeContainer } from "./styles";
+import closeImg from '../../assets/close.svg'
+import IncomeImg from '../../assets/income.svg'
+import OutcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
   openModalElement: any;
@@ -24,8 +28,45 @@ export function NewTransactionModal({ openModalElement }: NewTransactionModalPro
       <Modal
         isOpen={isModalOpen}
         onRequestClose={handleCloseModal}
+        overlayClassName='react-modal-overlay'
+        className='react-modal-content'
       >
-        <h2>Cadastrar Transação</h2>
+        <button
+          type="button"
+          className="react-modal-close"
+          onClick={handleCloseModal}
+        >
+          <img src={closeImg} alt="Fechar modal" />
+        </button>
+        <Container>
+          <h2>Cadastrar Transação</h2>
+
+          <input
+            placeholder="Título"
+          />
+
+          <input
+            type="number"
+            placeholder="Valor"
+          />
+
+          <TransactionTypeContainer>
+            <button >
+              <img src={IncomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </button>
+            <button>
+              <img src={OutcomeImg} alt="Entrada" />
+              <span>Saída</span>
+            </button>
+          </TransactionTypeContainer>
+
+          <input
+            placeholder="Categoria"
+          />
+
+          <button type="submit">Cadastrar</button>
+        </Container>
       </Modal>
     </>
   )
