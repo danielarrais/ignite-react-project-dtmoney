3f8717b9f05dfa535c992518d7ff9c11b0e8ebb0
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-02-07 23:01:18 -0300

    Removed unecessary imports

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
 import LogoImg from '../../assets/logo.svg'
 import { NewTransactionModal } from '../NewTransactionModal';
 import { Container, Content } from './styles'
