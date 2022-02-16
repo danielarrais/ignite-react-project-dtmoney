4dd7481df8be7c4a3e11afc99ecf819213c02e9d
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-02-15 22:38:46 -0300

    Close modal on created transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -45,6 +45,7 @@ export function NewTransactionModal({ openModalElement }: NewTransactionModalPro
     }
 
     api.post('/transactions', transaction);
+    setIsModalOpen(false);
   }
 
   return (
